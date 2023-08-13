3b04664e3363b0f6af8866daf9d083252f5cae3f
duexcoast <conor.ux@gmail.com>
2023-08-13 19:48:26 -0400

    conditionally add homebrew to PATH on arm64 macs

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -3,28 +3,32 @@ export VISUAL=nvim
 
 # Default editor nvim
 export EDITOR="$VISUAL"
-export PATH="/usr/local/sbin:$PATH"
-
 
 export PATH="$HOME/.npm-global/bin:$PATH"
 export GOPATH="$HOME/go"
-export PATH="$GOPATH/bin:$PATH" 
+export PATH="$GOPATH/bin:$PATH"
 export DOOM="$HOME/.emacs.d/bin/doom:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="/usr/local/protobuf/bin:$PATH"
-export PATH="/opt/homebrew/bin:$PATH"
+
+# Add homebrew programs to PATH, only on macs.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ "$CPUTYPE" == "arm64" ]]; then
+        export PATH="/opt/homebrew/bin:$PATH"
+    fi
+fi
 
 
 # Source zsh-completions plugin
 # This must be run before `source "$ZSH/oh-my-zsh.sh"`
 fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
 
-# Locale 
-# I have locale set in the alacritty config. 
+# Locale
+# I have locale set in the alacritty config.
 # export LANG="en_US.UTF-8"
 # export LANGUAGE="en_US.UTF-8"
 
-# Zsh 
+# Zsh
 export ZSH="$HOME/.oh-my-zsh"
 
 # fzf.
