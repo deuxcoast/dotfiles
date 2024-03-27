45960d38d013ca01559e9f25786e5ed1f46da6df
duexcoast <conor.ux@gmail.com>
2024-03-27 12:06:48 -0400

    enable null-ls format on save via autocmd

--- nvim/.config/nvim/lua/config/lsp/init.lua
+++ nvim/.config/nvim/lua/config/lsp/init.lua
@@ -118,6 +118,7 @@ local filetype_attach = setmetatable({
     end,
 
     typescript = function()
+        -- autocmd_format(false)
         autocmd_format(false, function(client)
             return client.name ~= "tsserver"
         end)
@@ -451,10 +452,43 @@ end
 -- end
 --]]
 
+local async_formatting = function(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+    vim.lsp.buf_request(
+        bufnr,
+        "textDocument/formatting",
+        vim.lsp.util.make_formatting_params {},
+        function(err, res, ctx)
+            if err then
+                local err_msg = type(err) == "string" and err or err.message
+                -- you can modify the log message / level (or ignore it completely)
+                vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+                return
+            end
+
+            -- don't apply results if buffer is unloaded or has been modified
+            if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+                return
+            end
+
+            if res then
+                local client = vim.lsp.get_client_by_id(ctx.client_id)
+                vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+                vim.api.nvim_buf_call(bufnr, function()
+                    vim.cmd "silent noautocmd update"
+                end)
+            end
+        end
+    )
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 require("null-ls").setup {
     sources = {
         -- require("null-ls").builtins.formatting.stylua,
-        -- require("null-ls").builtins.diagnostics.eslint,
+        require("null-ls").builtins.diagnostics.eslint_d,
         -- require("null-ls").builtins.completion.spell,
         -- require("null-ls").builtins.diagnostics.selene,
         require("null-ls").builtins.formatting.prettierd,
@@ -464,6 +498,18 @@ require("null-ls").setup {
         -- require("null-ls").builtins.formatting.uncrustify,
         require("null-ls").builtins.formatting.clang_format,
     },
+    on_attach = function(client, bufnr)
+        if client.supports_method "textDocument/formatting" then
+            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+            vim.api.nvim_create_autocmd("BufWritePost", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    async_formatting(bufnr)
+                end,
+            })
+        end
+    end,
 }
 
 return {
